b76894c3032cc4c8aaef0dabf4e8d78839c933cb
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-23 22:44:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 require 'rspec'
 
-def fizzBuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
-  else
-    number
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+   return "FizzBuzz"
+  end
+  if int % 5 == 0
+   return "Buzz"
+  end
+  if int % 3 == 0
+   return "Fizz"
   end
 end
